7d7104f58ec8174544d36fe04059e745aab7218e
Joshua Chinchilla <JoshuaChinchilla91@gmail.com>
2020-08-20 20:26:29 -0700

    another attempt at fixing error

--- data/seeds/002-items.js
+++ data/seeds/002-items.js
@@ -1,11 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   return knex('items').insert([
-    { item_name: 'xbox', description: 'Some description', price: '$50', user_id: 1 },
-    { item_name: 'drone', description: 'yo waddup', price: '$1000', user_id: 1},
-    { item_name: 'VR headset', description: 'You know', price: '$50', user_id: 2},
-    { item_name: 'tv', description: 'A big one', price: '$14', user_id: 3},
-    { item_name: 'drone', description: 'yo waddup', price: '$1000', user_id: 4},
-    { item_name: 'drone', description: 'yo waddup', price: '$1000', user_id: 2}
+    { item_name: 'xbox', description: 'It\'s an xbox!', price: '$50', user_id: 1, img_src: 'https://news.xbox.com/en-us/wp-content/uploads/sites/2/2019/12/XboxSeriesXHERO.jpg?fit=1920%2C1080' },
+    { item_name: 'TV', description: 'TV to go with the xbox', price: '$10', user_id: 1}
   ])
 }
