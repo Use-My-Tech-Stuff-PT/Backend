874b9ff5fc4be7766d259e886eaea7861686f5d3
Joshua Chinchilla <JoshuaChinchilla91@gmail.com>
2020-08-27 09:33:05 -0700

    another fix

--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 const secrets = require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
-    const [authType, token] = req.headers.authorization
+    const token = req.headers.authorization;
     console.log('token:', token);
 
     if (token) {
         jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
             if (err) {
-                res.status(401).json({ message: 'Token is invalid' })
+                res.status(401).json({ message: 'Token is invalid', error: err.message })
             } else {
                 req.decodedToken = decodedToken;
                 next();
