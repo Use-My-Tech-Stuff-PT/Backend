21cf126fe0f2149c6f8ce3cb2b0f3aee10b36f5b
Joshua Chinchilla <JoshuaChinchilla91@gmail.com>
2020-08-28 17:52:56 -0700

    Another try at a fix

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const helmet = require('helmet');
 
 const UsersRouter = require('./users/users-router.js');
@@ -6,18 +7,21 @@ const ItemsRouter = require('./items/items-router.js');
 
 const server = express();
 
-// server.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-// })
+
 
 server.use(helmet());
+server.use(cors());
 server.use(express.json());
 
 server.use("/", express.static(__dirname + "/documentation"));
 
 server.use('/api/users', UsersRouter);
 server.use('/api/items', ItemsRouter);
+
+server.options('/api/items/:id', cors()) // enable pre-flight request for delete
+server.put('/api/items/:id', cors(), function (req, res, next){
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+});
 
 server.get('/', function (req, res) {
     res.sendfile(__dirname + './documentation/index.html');
