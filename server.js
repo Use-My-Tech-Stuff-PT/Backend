616ab26f504758f6462ec7c3eb93cd780194ffbd
Joshua Chinchilla <JoshuaChinchilla91@gmail.com>
2020-08-28 20:46:57 -0700

    Joshua-Chinchilla

--- server.js
+++ server.js
@@ -13,15 +13,22 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 server.use("/", express.static(__dirname + "/documentation"));
 
 server.use('/api/users', UsersRouter);
 server.use('/api/items', ItemsRouter);
 
-server.options('/api/items/:id', cors()) // enable pre-flight request for delete
-server.put('/api/items/:id', cors(), function (req, res, next){
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-});
+// server.options('/api/items/:id', cors()) // enable pre-flight request for delete
+// server.put('/api/items/:id', cors(), function (req, res, next){
+//     res.json({msg: 'This is CORS-enabled for all origins!'})
+// });
 
 server.get('/', function (req, res) {
     res.sendfile(__dirname + './documentation/index.html');
